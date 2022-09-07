cfe4c308c41dce88093db399e402f774f29bc185
louis barlas <louisjdbarlas@gmail.com>
2022-09-07 14:54:51 +0100

    countries cooked counter finished, only countries with a unique id counted, not done by validations

--- app/controllers/cooked_dishes_controller.rb
+++ app/controllers/cooked_dishes_controller.rb
@@ -10,15 +10,5 @@ class CookedDishesController < ApplicationController
     else
       redirect_to root_path
     end
-
-    # @review = Review.new(review_params)
-    # @list = List.find(params[:list_id])
-    # @review.list = @list
-    # if @review.save
-    #   redirect_to list_path(@list)
-    # else
-    #   @bookmark = Bookmark.new
-    #   render 'lists/show', status: :unprocessable_entity
-    # end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @cooked_dishes = @user.cooked_dishes
+    #  countries cooked start
+    @countries = []
+    current_user.cooked_dishes.each do |cd|
+      @countries << cd.dish.country_id
+    end
+    # countries cooked end
+    @cooked_dishes = @user.cooked_dishes # cooked dishes counter
   end
 end
