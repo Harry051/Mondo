b912687926fee44684be73ccad9cdbd17d6c044c
Ling Fung Chan <justinlfchan@gmail.com>
2022-09-07 11:36:14 +0100

    setup for db seed & reviews validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :dish
   belongs_to :user
+
+  validates :content, presence: true, length: { minimum: 10 }
+  validates :rating, presence: true, numericality: { greater_than_equal_to: 1, less_than_equal_to: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'cooked_dishes/create'
   devise_for :users
   root to: "pages#home"
 
@@ -10,6 +9,7 @@ Rails.application.routes.draw do
   resources :dishes, only: [:show] do
     resources :cooked_dishes, only: [:create]
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
