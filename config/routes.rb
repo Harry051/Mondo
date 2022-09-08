b561b00ed7a76549964617c926aab925b4a53cf6
Jason Armstrong <jasnarmstrng@gmail.com>
2022-09-08 13:12:35 +0100

    Favourite route added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  # independent route for favs
+  resources :favourites, only: [:index]
+
   resources :users, only: [:show] do
     # get '/profile', to: 'users#profile'
   end
@@ -13,9 +16,4 @@ Rails.application.routes.draw do
     resources :favourites, only: [:create]
     resources :reviews, only: %i[new create]
   end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
