723aa2399e5685169a8d323a95f6e94f163292d2
Harry Cundall <harry@cundalls.com>
2022-09-09 12:49:53 +0100

    Hi-res photos and calories column implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_145309) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_114150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_145309) do
     t.bigint "country_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "calories"
     t.index ["country_id"], name: "index_dishes_on_country_id"
   end
 
