e1ae6055bf61f9733c4c4654862a0a9235f5b065
Ling Fung Chan <justinlfchan@gmail.com>
2022-09-12 22:11:54 +0100

    updated bottom-nav-bar-btn

--- app/javascript/controllers/heart_controller.js
+++ app/javascript/controllers/heart_controller.js
@@ -2,14 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="heart"
 export default class extends Controller {
-  connect() {
-  }
-
   static targets = ["heart"]
 
-  colorHeart(event) {
-    this.element.classList.remove('fa-regular');
-    this.element.classList.add('heart');
-    this.element.classList.add('fa-solid');
+  connect() {
+    const favourite = document.querySelector('.dish-heart-btn');
+    const favBtn = document.querySelector('.dish-fa-heart')
+
+
+    favourite.addEventListener('click', (event) => {
+      favBtn.classList.toggle('fa-regular');
+      favBtn.classList.toggle('fa-solid');
+    });
   }
+
+  // colorHeart(event) {
+  //   this.element.classList.toggle('fa-regular');
+  //   this.element.classList.toggle('fa-solid');
+  // }
 }
