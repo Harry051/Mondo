4ba6cff4f0959015a168f35d8d039a75892a52e7
Jason Armstrong <jasnarmstrng@gmail.com>
2022-09-12 16:28:38 +0100

    Refresh button for show page added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   def home
     @countries = Country.all
+    # Add category filter to the country
     if params[:category]
       @countries = @countries.select do |country|
         country.dishes.any? { |dish| dish.categories.include?(params[:category]) }
       end
     end
+
     @markers = @countries.map do |country|
       if country.geocoded?
         {
@@ -13,7 +15,6 @@ class PagesController < ApplicationController
           lng: country.longitude,
           # info_window: render_to_string(partial: "info_window", locals: {country: country}),
           image_url: "<a href=\"/dishes/#{params[:category] ? country.category_dishes(params[:category]).sample.id : country.dishes.sample.id}\"> <img src=\"#{country.photo.url}\" width=\"30\" height=\"30\" style=\"border-radius: 50%\"> </a>"
-
         }
       end
     end
