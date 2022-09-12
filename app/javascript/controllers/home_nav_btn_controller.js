11049cec2c409545d552cbf09edba0052bf2a238
Ling Fung Chan <justinlfchan@gmail.com>
2022-09-12 12:21:55 +0100

    update pages-home nav-menu-bar

--- app/javascript/controllers/home_nav_btn_controller.js
+++ app/javascript/controllers/home_nav_btn_controller.js
@@ -6,28 +6,19 @@ export default class extends Controller {
 
   connect() {
 
-    // const menuIcons = "<li class="home-tab-menu-item"><span class="home-tab-menu-icon"><i style="color: #261127" class="fa-solid fa-book-atlas fa-lg"></i></span><span class="home-tab-menu-text">Home</span></li>;
+    console.log('XX')
 
-    const menuBar = document.querySelector('.nav-btn');
-
-    menuBar.addEventListener('click', (event) => {
-      // console.log(event);
-      menuBar.classList.toggle('home-tab-menu');
-    });
-
+    const menuBar = document.querySelector('.home-nav');
+    const menuNav = document.querySelector('.nav-btn');
     // const menuItems = document.querySelectorAll('.home-tab-menu-item');
-    // let previousSelectedItem;
+    const menuItems = document.querySelector('.home-tab-menu-tabs');
 
-    // menuItems.forEach (item => {
-    //   item.addEventListener("click", () => {
-    //     previousSelectedItem?.classList.remove('home-tab-menu-active');
-    //     previousSelectedItem = item;
-    //     item.classList.add('home-tab-menu-active');
-    //   })
-    // })
-
-  // showMenu() {
-  //   console.log(this.menuTarget)
-  //   this.menuTarget.innerHTML = "<h2>Cooking History</h2> <hr> <p>1. Lasagna</p>"
-  // }
+    menuNav.addEventListener('click', (event) => {
+      menuBar.classList.toggle('home-tab-menu');
+      menuItems.classList.toggle('d-none');
+      // menuItems.forEach (item => {
+      //   item.classList.toggle('d-none');
+      // });
+    });
+  };
 }
