4a8a31ce99baec8bbcc3167bb891d99bc44e3854
Jason Armstrong <jasnarmstrng@gmail.com>
2022-09-13 11:18:59 +0100

    Added photo to dish review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :dish
   belongs_to :user
+  has_one_attached :photo # to attach images linked in Cloudinary
 
   validates :content, presence: true, length: { minimum: 10 }
   validates :rating, presence: true, numericality: { in: 1..5 }
