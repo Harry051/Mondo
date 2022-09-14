3c65684a10fb8eecb0ca7799da893182cb10e018
Ling Fung Chan <justinlfchan@gmail.com>
2022-09-14 12:51:44 +0100

    fixed dish-reviews

--- app/javascript/controllers/cuisine_reviews_controller.js
+++ app/javascript/controllers/cuisine_reviews_controller.js
@@ -2,10 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="cuisine-reviews"
 export default class extends Controller {
-  static targets = ["reviews", "caret"]
+  // static targets = ["reviews", "caret"]
 
   connect() {
-    console.log("hiii")
+    console.log("hello")
     const caretBtn = document.querySelector(".reviews-drop-btn");
     const reviewsContent = document.querySelector(".dish-show-reviews-item");
 
@@ -16,9 +16,9 @@ export default class extends Controller {
     })
   }
 
-  revealReviews() {
-    // this.reviewsTarget.classList.toggle("d-none");
-    // this.caretTarget.classList.toggle("fa-caret-down");
-    // this.caretTarget.classList.toggle("fa-caret-up");
-  }
+  // revealReviews() {
+  //   this.reviewsTarget.classList.toggle("d-none");
+  //   this.caretTarget.classList.toggle("fa-caret-down");
+  //   this.caretTarget.classList.toggle("fa-caret-up");
+  // }
 }
